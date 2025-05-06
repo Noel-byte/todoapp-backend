89eff951bdcdfff0a413924d76811d65f1331543
Noel <noeltesdev@gmail.com>
2025-05-06 13:38:49 -0600

    update

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors  from 'cors'
-import dotenv from 'dotenv'
-import todoRoutes from './routes/todoRouter.js'  
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import todoRoutes from './routes/todoRouter.js';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
 //middleware
-app.use(cors()) //enables cross-origin requests (cors - cross-origin resource sharing)
+app.use(cors()); //enables cross-origin requests (cors - cross-origin resource sharing)
 
-app.use(express.json())
+app.use(express.json());
 
 //mounting routers
-app.use('/api/todos',todoRoutes)
+app.use('/api/todos', todoRoutes);
 
 //handlers - routing
 
-app.get('/',(req,res)=>{
-    res.send('Welcome To The ToDo-List App!')
-})
+app.get('/', (req, res) => {
+  res.send('Welcome To The ToDo-List App!');
+});
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-.then(()=>{
-    console.log('MongoDB connected')
-    app.listen(process.env.PORT||5000, ()=>console.log(`server running on port ${process.env.PORT||5000}`))
-})
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('MongoDB connected');
+  app.listen(process.env.PORT || 5000, () =>
+    console.log(`server running on port ${process.env.PORT || 5000}`)
+  );
+});
