7490e251cd9c32f866abbf9f020418d44e25bf23
Noel <noeltesdev@gmail.com>
2025-05-08 13:26:56 -0600

    modified both userController.js and userRoutes.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,28 @@
+//@desc Register new user
+//@route   POST /api/users
+//@access Public
+
 export const registerUser = (req,res)=>{
     
     res.json({message:'Register user'})
+    
+}
+
+//@desc Authenticaate a user
+//@route   POST /api/login
+//@access Public
+export const loginUser = (req,res)=>{
+    
+    res.json({message:'Logi user'})
+    
+}
+
+//@desc  Get user data
+//@route   GET /api/users/me
+//@access Public
+export const getUser = (req,res)=>{
+    
+    res.json({message:'get user'})
     
 }
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { registerUser } from '../controllers/userController.js'
+import { loginUser } from '../controllers/userController.js'
+import { getUser } from '../controllers/userController.js'
 
 
 const router = express.Router()
 
 router.post('/',registerUser)
+router.post('/',loginUser)
+router.post('/',getUser)
 
 export default router
