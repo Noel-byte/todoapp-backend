9bfbe277c250d4bcac130f438b30c64db9bf32e8
Noel <noeltesdev@gmail.com>
2025-05-08 13:34:54 -0600

    modified userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,11 @@
 import express from 'express'
-import { registerUser } from '../controllers/userController.js'
-import { loginUser } from '../controllers/userController.js'
-import { getUser } from '../controllers/userController.js'
+import { registerUser,loginUser,getUser } from '../controllers/userController.js'
 
 
 const router = express.Router()
 
 router.post('/',registerUser)
 router.post('/login',loginUser)
-router.post('/user',getUser)
+router.get('/user',getUser)
 
 export default router
