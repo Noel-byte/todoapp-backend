a99425f6b3dc4a61149d3b234e0a19c193bc8c38
Noel <noeltesdev@gmail.com>
2025-05-08 18:35:39 -0600

    modified backend

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,18 +1,65 @@
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcryptjs'
-import asyncHandler from 'express-async-handler'
-import Todo from '../models/Todo.js'
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import asyncHandler from 'express-async-handler';
+import Todo from '../models/Todo.js';
 
-export const createTodo =  asyncHandler ( async (req,res)=>{
-    const {text} = req.body
-    const newTask = new Todo({text}) //create new task
-    const saveTask = await newTask.save()
+export const createTodo = asyncHandler(async (req, res) => {
+  const { text } = req.body;
+  const newTask = new Todo({ text }); //create new task
+  const saveTask = await newTask.save();
 
-    if(saveTask){
-        res.status(201).json(saveTask)
-    }else{
-        res.status(400)
-        throw new Error ('Invalid data')
-    }
- 
-})
+  if (saveTask) {
+    res.status(201).json(saveTask);
+  } else {
+    res.status(400);
+    throw new Error('Invalid data');
+  }
+});
+
+export const filterTodo = asyncHandler(async (req, res) => {
+  let query = {};
+
+  const { status } = req.params;
+
+  if (!status) {
+    res.status(400);
+    throw new Error('Could not retrieve todos');
+  }
+
+  if (status === 'complete') {
+    query.completed = true;
+  } else if (status === 'incomplete') {
+    query.completed = false;
+  }
+  const todos = await Todo.find(query); //retrieve all documents based on the query
+  res.status(200).json(todos); //send them back as json
+});
+
+export const deleteTodo = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(400);
+    throw new Error('could not delete item');
+  }
+  await Todo.findByIdAndDelete(id);
+  res.status(200).json({ message: 'item deleted successfully' });
+});
+
+export const updateTodo = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(400);
+    throw new Error('could not update todo');
+  }
+  await Todo.findByIdAndUpdate(id, req.body);
+  res.status(200).json({ message: 'updated successfully' });
+});
+
+export const deleteAll = asyncHandler(async (req, res) => {
+  if (await Todo.deleteMany({})) {
+    res.status(200).json({ message: 'tasks cleared' });
+  } else {
+    res.status(400);
+    throw new Error('could not clear');
+  }
+});
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,73 +1,23 @@
 import express from 'express'
 import Todo from '../models/Todo.js'
-import { createTodo } from '../controllers/todoController.js'
+import { createTodo,filterTodo,deleteTodo,updateTodo, deleteAll } from '../controllers/todoController.js'
 
 const router = express.Router()
-
 
 
 // POST api/todos  -- create new task
 router.post('/',createTodo)
 
-// router.post('/', async (req,res)=>{
+//GET  api/todos -- filter todos
+router.get('/:status',filterTodo)
 
-//     try{
-        
-//     }catch(err){
-       
-//     }
-  
-// })
+//DELETE api/todos -- delete todos
+router.delete('/:id',deleteTodo)
 
-router.get('/:status',async(req,res)=>{
-    let query ={}
-    try{
-        const{status} = req.params
-        if(status==='complete'){
-            query.completed=true
-        }
-       else if(status==='incomplete'){
-            query.completed=false
-        }
-        const todos = await Todo.find(query); //retrieve all documents based on the query
-        res.status(200).json(todos) //send them back as json
-    }catch(error){
-        console.error('Error fetching todos:', error)
-        res.status(500).json({message:'Server Error: could not retrieve todos'})
-    }
-    
-})
+//PUT api/todos -- update todos
+router.put('/:id',updateTodo)
 
-router.delete('/:id',async (req,res)=>{
-    try{
-        const {id} = req.params
-        await Todo.findByIdAndDelete(id)
-        res.status(200).json({message:'item deleted successfully'})
-    }catch(error){
-        console.error('error deleting item')
-        res.status(500).json({message:'could not delete item'})
-    }
-   
-})
-
-router.put('/:id',async(req,res)=>{
-    try{
-        const {id} = req.params
-        await Todo.findByIdAndUpdate(id,req.body)
-        res.status(200).json({message:'updated successfully'})
-    }catch(error){
-        console.error('error updating item')
-        res.status(500).json({message:'could not update item'})
-    }
-})
-
-router.delete('/',async(req,res)=>{
-    try{
-        await Todo.deleteMany({})
-        res.status(200).json({message:'tasks cleared'})
-    }catch(error){
-        res.status(500).json({message:'could not clear'})
-    }
-})
+//DELETE   api/todos -- delete all todos
+router.delete('/',deleteAll)
 
 export default router
