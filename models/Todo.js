21d1be85be826fa65b47dc10bbe70b0d15de6dc9
Noel <noeltesdev@gmail.com>
2025-05-08 18:37:51 -0600

    modified Todo.js

--- models/Todo.js
+++ models/Todo.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 const todoSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
+  // user: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   required: true,
+  //   ref: 'User',
+  // },
   text: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   completed: { type: Boolean, default: false },
