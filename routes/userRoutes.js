d30bd8f70becace7a6b5c95dbc02c79909aa1c49
Noel <noeltesdev@gmail.com>
2025-05-09 21:47:48 -0600

    modified backend

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.post('/register',registerUser)
+router.post('/',registerUser)
 router.post('/login',loginUser)
 router.get('/user',protect,getUser)
 
